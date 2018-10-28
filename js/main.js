3401f2a6dab204cc305d65d5fb8ecb9d0fe7510e
Redvanisation <redvanisation@gmail.com>
2018-10-28 10:13:51 -0400

    adding js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 const div = document.getElementById("chat-bot");
 
-console.log(div)
+var prompt = require('prompt-sync')();
+var AssistantV1 = require('watson-developer-cloud/assistant/v1');
+
+
+var assistant = new AssistantV1({
+	version: '{2018-09-20}',
+	iam_apikey: '{cfe89b8c-ffca-4e51-b69e-a306492d17dc}',
+	url: '{https://gateway.watsonplatform.net/assistant/api/v1/workspaces/cfe89b8c-ffca-4e51-b69e-a306492d17dc/message/}'
+});
+
+fetch(assistant).then(res => res.json()).then(res => console.log(res)).catch(err => console.log(err));
+
+
